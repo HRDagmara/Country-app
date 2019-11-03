d590c4b445f8cf52a9ad51c8f179a465c97b7a16
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-11-03 21:56:58 +0100

    add changes

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from './DevTools';
+import routes from './routes';
+import { getCountries } from './actions/actions-countries';
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory} routes={routes}>
+                <div>
+                    <h1>Inicjalizacja projektu</h1>
+                    <DevTools />
+                </div> 
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
