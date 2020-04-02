ca18b910ba010d266db16df3fc73a38a8ca82a88
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2020-04-02 14:02:41 +0200

    Updates

---
+++ precache-manifest.152855661ce636b7db4b33a40d012349.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b51544a4b79f579f799d86a34febbf0",
+    "url": "/Country-app/index.html"
+  },
+  {
+    "revision": "896845bdfaef2b652d32",
+    "url": "/Country-app/static/css/2.06a7aa02.chunk.css"
+  },
+  {
+    "revision": "896845bdfaef2b652d32",
+    "url": "/Country-app/static/js/2.71ce61bc.chunk.js"
+  },
+  {
+    "revision": "166ba6f629629fd95f39",
+    "url": "/Country-app/static/js/main.981c8403.chunk.js"
+  },
+  {
+    "revision": "a8b89fcd1b8d0ac6ff3a",
+    "url": "/Country-app/static/js/runtime~main.21bec543.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Country-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Country-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Country-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Country-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Country-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
